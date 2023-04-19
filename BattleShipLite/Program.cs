8f07c26f9f936053b060d16a9c01e8ddffa430b8
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-04-18 20:28:51 -0400

    Initial commit

---
+++ BattleShipLite/Program.cs
@@ -0,0 +1,186 @@
+﻿using BattleShipLiteLibrary;
+using BattleShipLiteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            PlayerInfoModel activePlayer = CreatePlayer("Player 1");
+            PlayerInfoModel nonActivePlayer = CreatePlayer("Player 2");
+            PlayerInfoModel winner = null;
+
+            do
+            {
+                DisplayShotGrid(activePlayer);
+
+                RecordPlayerShot(activePlayer, nonActivePlayer);
+
+                bool doesGameContinue = GameLogic.PlayerStillActive(nonActivePlayer);
+                if (doesGameContinue)
+                {
+                    // Swap AP/NAP
+                    (activePlayer, nonActivePlayer) = (nonActivePlayer, activePlayer);
+                }
+                else
+                {
+                    winner = activePlayer;
+                }
+
+                // TODO Display result
+
+            } while (winner == null);
+
+            IdentifyWinner(winner);
+
+            Console.ReadLine();
+        }
+
+        private static void IdentifyWinner(PlayerInfoModel winner)
+        {
+            Console.WriteLine($"Congrats to {winner.UsersName} for winning!");
+            Console.WriteLine($"{winner.UsersName} took {GameLogic.GetShotCount(winner)} shots.");
+        }
+
+        private static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel nonActivePlayer)
+        {
+            bool isValidShot = false;
+            string row = "z";
+            int column = 0;
+
+            do
+            {
+                string shot = AskForShot(activePlayer);
+                (row, column) = GameLogic.SplitShotIntoRowAndColumn(shot);
+                isValidShot = GameLogic.ValidateShot(row, column, activePlayer);
+
+                if (!isValidShot)
+                {
+                    Console.WriteLine("Invalid shot location. Please try again.");
+                }
+
+            } while (!isValidShot);
+
+            bool isAHit = GameLogic.IdentifyShotResult(nonActivePlayer, row, column);
+
+            GameLogic.MarkShotResult(activePlayer, row, column, isAHit);
+            GameLogic.UpdateShipCount(nonActivePlayer, row, column);
+            DisplayShotResult(row, column, isAHit);
+            Console.Clear();
+        }
+
+        private static void DisplayShotResult(string row, int column, bool isAHit)
+        {
+            if (isAHit)
+            {
+                Console.WriteLine($"{row}{column} is a direct hit!");
+            }
+            else
+            {
+                Console.WriteLine($"{row}{column} is a miss.");
+            }
+            Console.Write("Hit enter to continue...");
+            Console.ReadLine();
+        }
+
+        private static string AskForShot(PlayerInfoModel player)
+        {
+            Console.WriteLine($"{player.UsersName}, Please enter your shot.");
+            string output = Console.ReadLine();
+            return output;
+        }
+
+        private static void DisplayShotGrid(PlayerInfoModel activePlayer)
+        {
+            string currentRow = activePlayer.ShotGrid[0].SpotLetter;
+
+            foreach (var gridSpot in activePlayer.ShotGrid)
+            {
+                if (gridSpot.SpotLetter != currentRow)
+                {
+                    Console.WriteLine();
+                    currentRow = gridSpot.SpotLetter;
+                }
+
+                if (gridSpot.Status == GridSpotStatus.Empty)
+                {
+                    Console.Write($" {gridSpot.SpotLetter}{gridSpot.SpotNumber}");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Hit)
+                {
+                    Console.Write(" X ");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Miss)
+                {
+                    Console.Write(" O ");
+                }
+                else
+                {
+                    Console.Write(" ? ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to BattleShip Lite");
+            Console.WriteLine("created by Corey Jordan");
+            Console.WriteLine();
+        }
+
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            Console.WriteLine($"Player information for {playerTitle}");
+
+            // Ask the user for their name.
+            output.UsersName = AskForUsersName();
+
+            // Load up the shot grid.
+            GameLogic.InitializeGrid(output);
+
+            // Ask the user for their 5 ship placements
+            PlaceShips(output);
+
+            // Clear
+            Console.Clear();
+
+            return output;
+        }
+
+        private static string AskForUsersName()
+        {
+            Console.Write("What is your name? ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            do
+            {
+                Console.Write($"Where do you want to place ship number {model.ShipLocations.Count + 1}? ");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (!isValidLocation)
+                {
+                    Console.WriteLine("That was not a valid location. Please try again.");
+                }
+
+            } while (model.ShipLocations.Count < 5);
+        }
+    }
+}
---
+++ BattleShipLiteLibrary/GameLogic.cs
@@ -0,0 +1,192 @@
+﻿using BattleShipLiteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipLiteLibrary
+{
+    public static class GameLogic
+    {
+        public static int GetShotCount(PlayerInfoModel winner)
+        {
+            int shotCount = 0;
+            foreach (GridSpotModel spot in winner.ShotGrid)
+            {
+                if (spot.Status != GridSpotStatus.Empty)
+                {
+                    shotCount++;
+                }
+            }
+            return shotCount;
+        }
+
+        public static bool IdentifyShotResult(PlayerInfoModel nonActivePlayer, string row, int column)
+        {
+            bool isHit = false;
+            
+            foreach (GridSpotModel spot in nonActivePlayer.ShipLocations)
+            {
+                if (spot.SpotLetter == row && spot.SpotNumber == column)
+                {
+                    isHit = true;
+                }
+            }
+            return isHit;
+        }
+
+        public static void InitializeGrid(PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A", "B", "C", "D", "E"
+            };
+
+            List<int> numbers = new List<int>
+            {
+                1, 2, 3, 4, 5
+            };
+
+            foreach (var letter in letters)
+            {
+                foreach (var number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+            }
+        }
+
+        public static void MarkShotResult(PlayerInfoModel activePlayer, string row, int column, bool isAHit)
+        {
+            foreach (GridSpotModel spot in activePlayer.ShotGrid)
+            {
+                if (spot.SpotLetter == row && spot.SpotNumber == column && isAHit)
+                {
+                    spot.Status = GridSpotStatus.Hit;
+                }
+                else if (spot.SpotLetter == row && spot.SpotNumber == column && !isAHit)
+                {
+                    spot.Status = GridSpotStatus.Miss;
+                }
+            }
+        }
+
+        public static bool PlaceShip(PlayerInfoModel model, string location)
+        {
+            bool isValid = true;
+            (string row, int column) = SplitShotIntoRowAndColumn(location);
+
+            if (row[0] < 65 || row[0] > 69)
+            {
+                isValid = false;
+            }
+            else if (column < 1 || column > 5)
+            {
+                isValid = false;
+            }
+            else
+            {
+                foreach (GridSpotModel gridSpot in model.ShipLocations)
+                {
+                    if (gridSpot.SpotLetter == row && gridSpot.SpotNumber == column)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isValid)
+            {
+                GridSpotModel shipSpot = new GridSpotModel() { SpotLetter = row, SpotNumber = column };
+                model.ShipLocations.Add(shipSpot);
+            }
+            return isValid;
+        }
+
+        public static bool PlayerStillActive(PlayerInfoModel nonActivePlayer)
+        {
+            bool stillHasShips = false;
+            foreach (GridSpotModel spot in nonActivePlayer.ShipLocations)
+            {
+                if (spot.Status != GridSpotStatus.Sunk)
+                {
+                    stillHasShips = true;
+                    break;
+                }
+            }
+            return stillHasShips;
+        }
+
+        public static (string row, int column) SplitShotIntoRowAndColumn(string shot)
+        {
+            string rowString = "z";
+            int columnNumber = -1;
+
+            if (shot.Length == 2 &&
+                char.IsLetter(shot[0]) &&
+                char.IsNumber(shot[1]))
+            {
+                rowString = shot.Substring(0, 1).ToUpper();
+                columnNumber = int.Parse(shot.Substring(1));
+            }
+
+            return (rowString, columnNumber);
+        }
+
+        public static void UpdateShipCount(PlayerInfoModel nonActivePlayer, string row, int column)
+        {
+            foreach (GridSpotModel spot in nonActivePlayer.ShipLocations)
+            {
+                if (spot.SpotLetter == row && spot.SpotNumber == column)
+                {
+                    spot.Status = GridSpotStatus.Sunk;
+                }
+            }
+        }
+
+        public static bool ValidateShot(string row, int column, PlayerInfoModel activePlayer)
+        {
+            bool isValid = true;
+
+            if (row[0] < 65 || row[0] > 69)
+            {
+                isValid = false;
+            }
+            else if (column < 1 || column > 5)
+            {
+                isValid = false;
+            }
+            else
+            {
+                foreach (GridSpotModel gridSpot in activePlayer.ShotGrid)
+                {
+                    if (gridSpot.SpotLetter == row &&
+                        gridSpot.SpotNumber == column &&
+                        (gridSpot.Status == GridSpotStatus.Miss ||
+                        gridSpot.Status == GridSpotStatus.Hit))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            return isValid;
+        }
+
+        private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
+        {
+            GridSpotModel spot = new GridSpotModel()
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+                Status = GridSpotStatus.Empty
+            };
+
+            model.ShotGrid.Add(spot);
+        }
+    }
+}
